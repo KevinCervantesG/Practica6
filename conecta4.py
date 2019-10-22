687aed2512831a32b4622289ae9506a83745e3d9
IsaiasRRendon <42423869+IsaiasRRendon@users.noreply.github.com>
2019-10-22 17:29:24 -0500

    Se agrega método main
    Método main

--- conecta4.py
+++ conecta4.py
@@ -46,3 +46,45 @@ def fin_juego(tablero):
 				return False
 	return True
 
+def jugar(player1,player2):
+	#se crea el tablero
+	Filas = 6
+	Columnas = 7
+	tablero = crear_tablero(Filas,Columnas)
+	
+	x = player1[0]
+	o = player2[0]
+	turno = x
+
+	while(True):
+		mostrar_tablero(tablero)
+		pos = int(input("Juega " + turno + " (elija numero de columna): "))
+		if(pos <= Columnas):
+
+			#agrego jugada
+			for i in range(Filas):
+				if(tablero[Filas-1-i][pos] == "*"):
+					tablero[Filas-1-i][pos] = turno
+					break
+
+			#verifico si turno ganó
+			if(es_ganador(tablero)):
+				print("\nGanó",turno)
+				break
+
+			#verifico si es empate
+			if(fin_juego(tablero)):
+				print("\nEmpate!")
+				break
+
+			#Cambio de turno
+			if(turno == x): turno = o
+			else: turno = x
+		elif(pos > Columnas):
+			print("es un valor entre 0 y 6")
+	mostrar_tablero(tablero)
+	
+print("Bienvenidos a conecta 4")
+player1 = input("Jugador 1 ingrese nombre")
+player2 = input("Jugador 2 ingrese nombre")
+jugar(player1,player2)
